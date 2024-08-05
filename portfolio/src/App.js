de986ef366605d636c8c63e55f301c88a5f53e11
Rodrigo Carvalho <144171797+Rodrigo5431@users.noreply.github.com>
2024-08-05 19:14:40 +0000

    bug

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,19 @@
 import './App.css';
-import './reset.css';
-import js from './assets/js.png';
-import html from './assets/HTML.png';
-import css from './assets/CSS.png';
 
 function App(){
 
     return(
         
     <body>
-        <header>
+      <header>
             <div className='images'>
-                <img src={js} alt="" />
-                <img src={html} alt="" />
-                <img src={css} alt="" />
             </div>
+            
         </header>
         <main>
             <h1>Olá, Meu nome é Rodrigo, e seja bem vindo ao meu Portfólio</h1>
         </main>
+ 
 
     </body>
 );
