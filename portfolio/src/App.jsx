b9a8cd86b007379a3421b0b0ebbab6dfddb018df
Rodrigo Carvalho <144171797+Rodrigo5431@users.noreply.github.com>
2024-08-05 18:56:51 +0000

    Begin

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import './reset.css';
+import js from './assets/js.png';
+import html from './assets/HTML.png';
+import css from './assets/CSS.png';
+
+function App(){
+
+    return(
+        
+    <body>
+        <header>
+            <div className='images'>
+                <img src={js} alt="" />
+                <img src={html} alt="" />
+                <img src={css} alt="" />
+            </div>
+        </header>
+        <main>
+            <h1>Olá, Meu nome é Rodrigo, e seja bem vindo ao meu Portfólio</h1>
+        </main>
+
+    </body>
+);
+}
+
+export default App;
